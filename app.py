c1ee2cba4618f06f1cc3957eb91e403b6c898f5c
Djamel Gharbi <degharbi@Djamels-MacBook-Pro.local>
2023-11-20 12:41:45 +0100

    first

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():    
+    df = pd.read_csv('historique-des-adjudications/data.csv', sep='|')
+    
+    return df
+
+st.title('Licitor Dashboard')
+data_load_state = st.text('Loading data...')
+data = load_data()
+data_load_state.text("Done! (using st.cache_data)")
+
+ads_number = data.ad_number.nunique()
+
+st.write(ads_number)
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
